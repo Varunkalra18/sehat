d372e6f99144d6cc628ea0115d350b5445f946d3
varun kalra <kalravarun1999@gmail.com>
2021-10-21 11:58:40 +0530

    package file

--- sehat/src/App.js
+++ sehat/src/App.js
@@ -1,9 +1,23 @@
+import React from 'react'
+import Homepage from './component/homepage/Homepage'
+import Login from './component/login/Login'
+import Register from './component/register/Register'
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/"><Homepage></Homepage></Route>
+          <Route path="/login"><Login></Login></Route>
+          <Route path="/register"><Register></Register></Route>
+        </Switch>
+      </Router>
+      {/* <Homepage></Homepage> */}
+      {/* <Login></Login> */}
+      {/* <Register></Register> */}
     </div>
   );
 }
